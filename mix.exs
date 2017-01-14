41202def8700e23fbf322cdfe943486a37ddb50d
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-01-14 14:18:25 +0700

    Optional cowboy dependency

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule PingPlug.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:cowboy, "~> 1.0.0"},
+    [{:cowboy, "~> 1.0.0", optional: true},
      {:plug, "~> 1.2.0"},
      {:ex_doc, "~> 0.14", only: :dev}]
   end
