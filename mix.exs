854c6ca6dacf4c215e23d9a4c9d045d74bd1a4e6
Cheab <cheab@omise.co>
2017-03-30 14:02:35 +0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PingPlug.Mixfile do
   def project do
     [
       app: :ping_plug,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.4",
       deps: deps(),
       package: package(),
