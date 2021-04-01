10d97f24a31927d1ab15fdbb9cb20d8db9259a25
Wiwatta Mongkhonchit <cheab@opn.ooo>
2021-04-01 10:09:16 +0700

    Remix

--- mix.exs
+++ mix.exs
@@ -5,31 +5,49 @@ defmodule PingPlug.Mixfile do
     [
       app: :ping_plug,
       version: "0.2.1",
-      elixir: "~> 1.8",
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
       package: package(),
+      aliases: aliases(),
       description: description(),
+      test_coverage: [tool: ExCoveralls],
       docs: [
         extras: ~W(README.md)
+      ],
+      preferred_cli_env: [
+        lint: :test,
+        coverage: :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [
+        plt_core_path: "priv/plts",
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
       ]
     ]
   end
 
   def application do
-    [applications: [:logger, :plug]]
+    [
+      extra_applications: [:logger]
+    ]
   end
 
   defp deps do
     [
-      {:cowboy, "~> 2.0", optional: true},
-      {:plug, "~> 1.7"},
-      {:ex_doc, ">= 0.19.1", only: :dev, runtime: false},
-      {:dialyxir, ">= 1.0.0-rc4", only: :dev, runtime: false}
+      {:credo, "~> 1.5", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.14", only: :test},
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
   defp package do
     [
+      name: :ping_plug,
       maintainers: ["Wiwatta  Mongkhonchit"],
       licenses: ["MIT"],
       links: %{
@@ -39,10 +57,20 @@ defmodule PingPlug.Mixfile do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   defp description do
     """
     A simple plug to echo message back, useful with healthcheck endpoint
     where you just need to response 200 ok.
     """
+  end
+
+  defp aliases do
+    [
+      lint: ["format --check-formatted", "credo"],
+      coverage: ["coveralls.html --raise"]
+    ]
   end
 end
